fce2720e289edc59903a5f7b44e5a888bde59dc5
3rnestocs <ernesto.contreras@drapshop.com>
2023-05-15 11:31:06 -0400

    Project setup

---
+++ RedditClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditClone
+//
+//  Created by Ernesto Jose Contreras Lopez on 15/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
